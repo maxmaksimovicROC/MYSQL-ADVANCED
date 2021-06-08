b34bbb7ae4956dc38a284fcbe4beb0d4869a1f84
maxmaksimovic <maxmaksimovicroc@gmail.com>
2021-06-08 11:18:35 +0200

    ;kj

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,12 @@
-
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES("willem", "wouter", "7A", "Albert Cuypstraat 3","1234AB", "2001-04-28","Amstelveen");
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES("willem", "wouter", "7A", "Albert Cuypstraat 3","1234AB", "2001-04-28","Amstelveen");
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES("willem", "wouter", "7A", "Albert Cuypstraat 3","1234AB", "2001-04-28","Amstelveen");
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES("willem", "wouter", "7A", "Albert Cuypstraat 3","1234AB", "2001-04-28","Amstelveen");
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES("willem", "wouter", "7A", "Albert Cuypstraat 3","1234AB", "2001-04-28","Amstelveen");
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES("willem", "wouter", "7A", "Albert Cuypstraat 3","1234AB", "2001-04-28","Amstelveen");
--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten(student_id,voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUE(11, hans, jansen, 9a, 2001-23-31, brussel)
 
+INSERT INTO studenten(student_id,voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUE(12, hans, jansen, 9a, 2001-23-31, brussel)
+
+INSERT INTO studenten(student_id,voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUE(13, hans, jansen, 9a, 2001-23-31, brussel)
