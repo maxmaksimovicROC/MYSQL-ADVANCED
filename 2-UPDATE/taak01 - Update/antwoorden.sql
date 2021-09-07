e5512c6b9f00cbf8a27c1072bd55be0a9f690ffb
maxmaksimovic <maxmaksimovicroc@gmail.com>
2021-09-07 10:53:27 +0200

    hb

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,5 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET woonplaats="Amstelveen";
 -- Opdracht 2 
 
--- Opdracht 3
-
--- Opdracht 4
-
--- Opdracht 5
--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,16 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET klas="9x"
+WHERE student_id <= 5
 -- Opdracht 2 
-
+UPDATE studenten2
+SET klas = "9z"
+WHERE student_id >7
 -- Opdracht 3
-
+UPDATE studenten2
+SET woonplaats = "Amsterdan"
+WHERE student_id <=2 OR <7
 -- Opdracht 4
-
+UPDATE studenten2
+SET woonplaats = "Uithoorn"
+WHERE klas = "9X"  AND woonplaats = "Amsterdam"
